41fff8270b878d8e4ba6d38c97754e72f8d3be5c
lmkai <793694022@qq.com>
2020-06-19 10:44:23 +0800

    提交

---
+++ src/main/java/com/lmk/RedisMainApplication.java
@@ -0,0 +1,11 @@
+package com.lmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedisMainApplication.class, args);
+    }
+}
